31da31f4fd43d4b19545f4eda5b84b916b4cd7db
dduubb <dalew@ksu.edu>
2019-08-08 09:59:43 -0500

    Update uspto-wdc.js

--- wdc/uspto-wdc.js
+++ wdc/uspto-wdc.js
@@ -175,12 +175,12 @@
                 if (table.tableInfo.id == "ipcData") {
                     var ipcCode;
                     for (var m = 0, lenm = patents[i].IPCs.length; m < lenm; m++) {
-                        ipcCode = patents[i].IPCs[m].ipc_section+patents[i].IPCs[m].ipc_class+patents[i].IPCs[m].ipc_subclass.toUpperCase();
+                        //ipcCode = patents[i].IPCs[m].ipc_section+patents[i].IPCs[m].ipc_class+patents[i].IPCs[m].ipc_subclass.toUpperCase();
                         if (patents[i].IPCs[m].ipc_section) {
                             tableData.push({
                                 "patent_number": patents[i].patent_number,
-                                "ipc_code": ipcCode,
-                                "cluster_by_ipc" : clusterLookup[ipcCode.toString()].Cluster,
+                                "ipc_code": patents[i].IPCs[m].ipc_section+patents[i].IPCs[m].ipc_class+patents[i].IPCs[m].ipc_subclass.toUpperCase(),
+                                "cluster_by_ipc" : clusterLookup[patents[i].IPCs[m].ipc_section+patents[i].IPCs[m].ipc_class+patents[i].IPCs[m].ipc_subclass.toUpperCase()].Cluster,
                             });
                         }
                     }
