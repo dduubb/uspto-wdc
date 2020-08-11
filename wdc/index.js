8ae34f1e3e9063726915d7b0e2c98e25838d5ea2
dduubb <dalew@ksu.edu>
2020-08-10 22:41:43 -0500

    sorting folders

--- wdc/index.js
+++ wdc/index.js
@@ -1,55 +1,55 @@
-/*global tableau, $*/
+/*global tableau*/
 import { loadCSV } from "./js/loadCsv.js"
 import { endpointConfig } from "./js/endpointConfig.js"
 import EndpointProto from "./js/Schema.js"
 import { layout } from "./js/layout.js"
-const urlHash = (window.location.hash.split("#")[1]) ? window.location.hash.split("#")[1] : "inventors";
-const myEndpoint = endpointConfig[urlHash];
-let myConnector = {}
+const urlHash = (window.location.hash.split("#")[1]) ? window.location.hash.split("#")[1] : "inventors"
+const myEndpoint = endpointConfig[urlHash]
 const url = `./endpoints/${myEndpoint['filename']}.csv`
-console.log(url)
-let usedColumns, endpointInst, tables
+let myConnector = {}
+let usedColumns, endpointInst
 init(url)
 
 async function init(url) {
     let dataObj = await loadCSV(url)
-    endpointInst = new EndpointProto(dataObj, myEndpoint['filename']);
+    endpointInst = new EndpointProto(dataObj, myEndpoint['filename'])
     layout.renderTable(endpointInst.full)
     layout.addOptions(endpointInst.filtered, "inventor_lastknown_city")
     layout.events(myEndpoint['title'], myEndpoint['docs'])
     usedColumns = endpointInst.usedColumns
-    tables = endpointInst.tables
+    submitButton.removeAttribute("disabled")
 }
 
-(() => {
-    myConnector = tableau.makeConnector();
+tableauInit()
+function tableauInit() {
+    myConnector = tableau.makeConnector()
     myConnector.getSchema = (schemaCallback) => {
-        schemaCallback(tables)
-    };
-
+        console.log(endpointInst.tables)
+        schemaCallback(endpointInst.tables)
+    }
     myConnector.getData = (table, doneCallback) => {
         let queryObj = JSON.parse(tableau.connectionData),
             filter = queryObj.customFilter || '"' + queryObj.filterKey + '":"' + queryObj.filterValue + '"',
-            finalURL = `https://www.patentsview.org/api/${myEndpoint['filename']}/query?q={${filter}}&o={"page":${queryObj.page},"per_page":${queryObj.per_page}}&f=[${[...usedColumns]}]`;
+            finalURL = `https://www.patentsview.org/api/${myEndpoint['filename']}/query?q={${filter}}&o={"page":${queryObj.page},"per_page":${queryObj.per_page}}&f=[${[...usedColumns]}]`
 
         fetch(finalURL).then((response) => {
             response.json().then((json) => {
                 let feat = json[myEndpoint['group']],
-                    tableData = endpointInst.prepareTables(feat, [myEndpoint['group']], table);
-                table.appendRows(tableData, [myEndpoint['group']]);
-                doneCallback();
+                    tableData = endpointInst.prepareTables(feat, [myEndpoint['group']], table)
+                table.appendRows(tableData, [myEndpoint['group']])
+                doneCallback()
             })
         }).catch((err) => console.error(`Error ${err}`))
-    };
+    }
+    tableau.registerConnector(myConnector)
+}
 
-    tableau.registerConnector(myConnector);
-})();
+layout.elements.homeTab.addEventListener('click', () => {
+    console.log(endpointInst, endpointInst.tables)
+    console.log(myConnector)
+})
 
 submitButton.addEventListener('click', () => {
-    myConnector.getSchema = (schemaCallback) => {
-        
-        schemaCallback(endpointInst.tables)
-    };
     let TCD = {
         page: document.getElementById('page').value,
         per_page: document.getElementById('per-page').value,
@@ -59,11 +59,11 @@ submitButton.addEventListener('click', () => {
         customFilter: document.getElementById('custom-filter').value,
         sortKey: document.getElementById('sort-key').value,
         sortValue: document.getElementById('sort-value').value,
-        connectionData: usedColumns
+        connectionData: usedColumns,
+        tables: endpointInst.tables
     }
-    tableau.connectionName = "Patent Connector";
-    tableau.connectionData = JSON.stringify(TCD);
-    tableau.submit();
-});
 
-
+    tableau.connectionName = "Patent Connector"
+    tableau.connectionData = JSON.stringify(TCD)
+    tableau.submit()
+})
--- wdc/js/layout.js
+++ wdc/js/layout.js
@@ -5,46 +5,38 @@ export const layout = {
         title: document.getElementById("title"),
         description: document.getElementById("description"),
         submitButton: document.querySelector('#submitButton'),
-        tableRows: document.getElementById("usptoRows")
+        tableRows: document.getElementById("usptoRows"),
+        homeTab: document.querySelector("#home-tab")
     },
-    events(endpoint,docsURL) {
+    
+    events(endpoint, docsURL) {
         title.innerHTML = `USPTO ${endpoint} endpoint`
         description.innerHTML = `This connector allows you to load data from the USPTO PatentsView API, Specifically the ${endpoint} endpoint as documented at <a href="${docsURL}" target="_blank">patentsview.org</a>`
-        submitButton.addEventListener('click', () => {
-            let tcd = {
-                page: document.getElementById('page').value,
-                per_page: document.getElementById('per-page').value,
-                page: document.getElementById('page').value,
-                filterKey: document.getElementById('filter-key').value,
-                filterValue: document.getElementById('filter-value').value,
-                customFilter: document.getElementById('custom-filter').value,
-                sortKey: document.getElementById('sort-key').value,
-                sortValue: document.getElementById('sort-value').value,
-            }
-            tableau.connectionName = "Patent Connector";
-            tableau.connectionData = JSON.stringify(tcd);
-            tableau.submit();
-        });
     },
+
     renderTable(full) {
         full.forEach((a, i) => {
-            document.getElementById("usptoRows").innerHTML += `<tr><th scope="row">${this.checkBox(a.skip,i)}</th><td>${a.id}<td>${a.description}</td></tr>`;
-        });
+            document.getElementById("usptoRows").innerHTML += `<tr><th scope="row">${this.checkBox(a.skip, i)}</th><td>${a.id}<td>${a.description}</td></tr>`
+        })
         document.querySelectorAll("#default-check").forEach((e) => e.addEventListener("click", (e) => {
+            console.log(full[e.target.value].skip)
+            console.log(full[e.target.value].skip)
             full[e.target.value].skip = !full[e.target.value].skip
         }))
     },
-    checkBox(a,i) {
+
+    checkBox(a, i) {
         return `<div class="form-check">
           <input class="form-check-input" type="checkbox" ${a ? "" : "checked"} value="${i}" id="default-check">
         </div>`
     },
+
     addOptions(list, def) {
         let innerHtml = ''
-        list.forEach((a) => { innerHtml += `<option ${a.id == def ? 'selected' : ''} value='${a.id}'>${a.id}</option>`; });
-        this.elements.filterKey.innerHTML = innerHtml;
-        this.elements.sortKey.innerHTML = innerHtml;
-        this.elements.filterKey.innerHTML = innerHtml;
+        list.forEach((a) => { innerHtml += `<option ${a.id == def ? 'selected' : ''} value='${a.id}'>${a.id}</option>` })
+        this.elements.filterKey.innerHTML = innerHtml
+        this.elements.sortKey.innerHTML = innerHtml
+        this.elements.filterKey.innerHTML = innerHtml
 
     }
 
